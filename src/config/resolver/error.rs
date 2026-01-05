84c3b664a0a4b3d4ddfad18f9b56f9140a5fb161
Tarek Galal <tare2.galal@gmail.com>
2026-01-05 15:02:35 +0100

    Fix variant resolution

--- src/config/resolver/error.rs
+++ src/config/resolver/error.rs
@@ -4,8 +4,6 @@ use thiserror::Error;
 pub enum ResolveError {
     #[error("'{0}' not found")]
     NotFound(String),
-    #[error("Provider '{0}' not supported")]
-    NotSupported(String),
     #[error("No groups defined in config")]
     NoGroups,
     #[error("Group '{0}' references '{1}' which is not found")]
